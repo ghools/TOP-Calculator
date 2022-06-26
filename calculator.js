d61bfd729a323d39034120d3e0cb3a0d22cb8679
ghools <santad06@yahoo.com>
2022-06-26 11:09:19 -0400

    Add math functions

--- calculator.js
+++ calculator.js
@@ -17,3 +17,18 @@ function divide (a, b) {
     let result = a / b;
     return result;
 }
+
+function operate (num1, num2, operator) {
+    if(operator = +){
+        add(num1, num2);
+    }
+    if(operator = -){
+        subtract(num1, num2);
+    }
+    if(operator = *){
+        multiply(num1, num2);
+    }
+    if(operator = /){
+        divide(num1, num2);
+    }
+}
