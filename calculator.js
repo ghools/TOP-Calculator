d7f823a26565ed45d2a928eaad9f221ed5b9bcc6
ghools <santad06@yahoo.com>
2022-06-25 22:12:46 -0400

    Add basic functions

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function add (a, b) {
+    let result = a + b;
+    return result;
+}
+
+function subtract (a, b) {
+    let result = a -b;
+    return result;
+}
+
+function multiply (a, b) {
+    let result = a * b; 
+    return result;
+}
+
+function divide (a, b) {
+    let result = a / b;
+    return result;
+}
